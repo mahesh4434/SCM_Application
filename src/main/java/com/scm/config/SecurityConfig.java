642ebdd626d81c43a1238e55c71fc77a2f13160e
mahesh4434 <maheshchaudhari511@gmail.com>
2025-12-28 14:29:52 +0530

    User form added

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -1,21 +1,26 @@
 package com.scm.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.scm.services.impl.SecurityCustomUserDetailService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfig {
@@ -67,7 +72,46 @@ public class SecurityConfig {
         // form Defualt login
         // Jat apkyala kuch bhi change krayche asel tr apn ithe yeu formlogin chya
         // related
-        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.formLogin(formLogin -> {
+
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/dashboard");
+            formLogin.failureForwardUrl("/login?error=true");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+            /*
+             * formLogin.failureHandler(new AuthenticationFailureHandler() {
+             * 
+             * @Override
+             * public void onAuthenticationFailure(HttpServletRequest request,
+             * HttpServletResponse response,
+             * AuthenticationException exception) throws IOException, ServletException {
+             * // TODO Auto-generated method stub
+             * throw new
+             * UnsupportedOperationException("Unimplemented method 'onAuthenticationFailure'"
+             * );
+             * }
+             * 
+             * });
+             * 
+             * formLogin.successHandler(new AuthenticationSuccessHandler() {
+             * 
+             * @Override
+             * public void onAuthenticationSuccess(HttpServletRequest request,
+             * HttpServletResponse response,
+             * Authentication authentication) throws IOException, ServletException {
+             * // TODO Auto-generated method stub
+             * throw new
+             * UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'"
+             * );
+             * }
+             * 
+             * });
+             */
+        }
+
+        );
         return httpSecurity.build();
 
     }
--- src/main/java/com/scm/controllers/pageController.java
+++ src/main/java/com/scm/controllers/pageController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +26,11 @@ public class pageController {
 
   @Autowired
   private UserService userService;
+
+  @GetMapping("/")
+  public String index() {
+    return "redirect:/home";
+  }
 
   @RequestMapping("/home")
   public String home(Model model) {
